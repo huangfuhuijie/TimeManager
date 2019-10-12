9c1c11b41ddc793ac9733744a33ef0cf8a4ada29
jueduihuangfu <787936777@qq.com>
2019-10-12 20:30:16 +0800

    创建项目开始

---
+++ feature/src/main/java/com/huangfu/scu/time_manager/feature/MainActivity.java
@@ -0,0 +1,13 @@
+package com.huangfu.scu.time_manager.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
